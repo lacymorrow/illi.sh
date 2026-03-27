5458edd62704704fe6ad8098e1eee129d6d8152f
Lacy Morrow <me@lacymorrow.com>
2026-03-27 12:52:26 -0400

    feat: add Umami analytics (illi.sh)

--- src/components/primitives/analytics.tsx
+++ src/components/primitives/analytics.tsx
@@ -1,7 +1,17 @@
 "use client";
 
+import Script from "next/script";
 import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
 
 export function Analytics() {
-  return <VercelAnalytics />;
+  return (
+    <>
+      <VercelAnalytics />
+      <Script
+        defer
+        src="https://analytics.lacy.sh/script.js"
+        data-website-id="20c60d66-27c3-4ca0-8bcf-996b99ff4e94"
+      />
+    </>
+  );
 }
